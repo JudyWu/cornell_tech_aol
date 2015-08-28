cb276979614b43ad73f739c2747d443113de2c0d
Judy Wu <judyzhaoxinwu@gmail.com>
2015-08-28 14:53:52 -0400

    Automated commit at 2015-08-28 18:53:52 UTC by middleman-deploy 1.0.0

--- javascripts/all.js
+++ javascripts/all.js
@@ -2,26 +2,26 @@
 $(document).ready(function() {
   $('#project-nav-button').click(function() {
     $('html, body').animate({
-      scrollTop: $('#projects-content').offset().top - 20}, 1000);
+      scrollTop: $('#projects-content').offset().top - 150}, 1000);
   });
   $('#project-arrow').click(function() {
     $('html, body').animate({
-      scrollTop: $('#projects-content').offset().top - 100}, 1000);
+      scrollTop: $('#projects-content').offset().top - 150}, 1000);
   });
 
   $('#about-nav-button').click(function() {
     $('html, body').animate({
-      scrollTop: $('#about-us-content').offset().top - 100}, 1000);
+      scrollTop: $('#about-us-content').offset().top - 150}, 1000);
   });
 
   $('#about-us-arrow').click(function() {
     $('html, body').animate({
-      scrollTop: $('#about-us-content').offset().top - 100}, 1000);
+      scrollTop: $('#about-us-content').offset().top - 150}, 1000);
   });
 
   $('#news-nav-button').click(function() {
     $('html, body').animate({
-      scrollTop: $('#news-content').offset().top - 200}, 1000);
+      scrollTop: $('#news-content').offset().top - 150}, 1000);
   });
 
 });
